d7ccec1033b9aab1314584cecf5c4057b378a83b
Siddarth Mehra <siddarthmehraajm@live.co.uk>
2022-05-26 03:05:29 +0530

    Latest_edits

--- frostCodeBase.py
+++ frostCodeBase.py
@@ -1,2 +1,3 @@
 def main():
     print( str('Welcome to frostCode👋'))
+    import importer
---
+++ importer.py
@@ -0,0 +1,11 @@
+#importing one file to import all the required modules
+import sys , time, os , re
+print('Imported sys , time, os , re')
+
+import maya.cmds as cmds
+print('Imported cmds')
+
+import pymel.core as pm
+print('Imported PyMel')
+
+
